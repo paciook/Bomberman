7af245481768e5c0d25e624a1e0182ddc49af5c1
fran <fran@localhost.localdomain>
2019-09-25 20:10:53 -0700

    Mejoras en el diagrama de clases y comienzo d bomb

---
+++ Codigo propio/bomba.py
@@ -0,0 +1,12 @@
+class Bomba:
+    def __init__(self, position, explotionRange):
+        self.position = position
+        self.explotionRange= explotionRange
+        self.explotionTime = 2 #seconds
+
+        self.plantar(position, explotionRange)
+
+    def plantar(self,position, explotionRange, explotionTime): #
+
+
+    def setPosition():
