55642203356ca74696a2921daf0b0ccda7f1cd20
fran <fran@localhost.localdomain>
2019-09-25 20:26:48 -0700

    Casi finish bomb

--- Codigo propio/bomba.py
+++ Codigo propio/bomba.py
@@ -1,12 +1,39 @@
+import time
+
+
 class Bomba:
     def __init__(self, position, explotionRange):
-        self.position = position
-        self.explotionRange= explotionRange
-        self.explotionTime = 2 #seconds
+        self.explotionTime = 2  # segundos
 
         self.plantar(position, explotionRange)
 
-    def plantar(self,position, explotionRange, explotionTime): #
+    def plantar(self, position, explotionRange, explotionTime):
 
+        self.setPosition(position)
+        self.setExplotionRange(explotionRange)
+        self.setExplotionTime(explotionTime)
 
-    def setPosition():
+        time.sleep(self.getExplotionTime())
+
+        self.explode()
+
+    def setPosition(pos):
+        self.position = pos
+
+    def getPosition():
+        return self.position
+
+    def setExplotionRange(exprng):
+        self.explotionRange = exprng
+
+    def getExplotionRange():
+        return self.explotionRange
+
+    def setExplotionTime(exptm):
+        self.explotionTime = exptm
+
+    def getExplotionTime():
+        return self.explotionTime
+
+    def explode():
+        
