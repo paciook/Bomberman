4d80f8bcd288cab54ebf7eebe6635d96ae62405c
Francisco Pacio <fpaciook@gmail.com>
2019-10-09 18:51:28 -0300

    Calculo de colisiones

--- Codigo propio/Calculate Collisions.py
+++ Codigo propio/Calculate Collisions.py
@@ -6,3 +6,19 @@ def collisions(thing):
         for column in range(0, dimension[1]): # suponiendo que 1 son las columnas
             if (map.dimension[position[0] + line][position[1] + column] > 1):
                 return True
+
+def calculateCollisions(thing):
+    dimension = thing.getDimension()
+    position = thing.getPosition()
+
+    for line in range(0, dimension[0]):
+        for column in range(0, dimension[1]):
+            array = map.getArray()
+
+            if (array[position[0 + line], position[1 + column]] += thing.array[line, column] > 1):
+                control.collision()
+            
+            array[position[0 + line], position[1 + column]] += thing.array[line, column]
+
+
+    map.setArray(array)
